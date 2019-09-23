39fc77e6368e19ea4d6e4e7c0f80ee3cd77c84ae
Jordi Pereira <jordipereiragude@gmail.com>
2019-09-23 18:10:17 +0000

    first try with maxflow (pending debugging)

--- scripts/sesion3.jl
+++ scripts/sesion3.jl
@@ -55,6 +55,34 @@ function bellmanFord(g::grafoCaminos,origen::Int64)
   return 0,l,tr
 end
 
+function dj(g::grafoCaminos,origen::Int64)
+    ∞=1000000
+    tr=zeros(Int64,g.vertices)
+    l=Array{Int64}(g.vertices)
+    fill!(l,∞)
+    l[origen]=0
+    Q=PriorityQueue()
+    enqueue!(Q,origen,0)
+    while isempty(Q)==false
+        v=dequeue!(Q)
+        for w in 1:g.vertices
+            if l[w] > (l[v]+g.longitud[v,w])
+                l[w]=l[v]+g.longitud[v,w]
+                if l[w]<0 #condición adicional para evitar loop infinito
+                  return l,tr
+                end
+                tr[w]=v
+                if haskey(Q,w)
+                    Q[w]=l[w]
+                else
+                    enqueue!(Q,w,l[w])
+                end
+            end
+        end
+    end
+    return l,tr
+end
+
 unGrafo=generarGrafoCaminos(25,10000)
 unGrafo.longitud[13,14]=1; unGrafo.longitud[14,15]=1; unGrafo.longitud[15,16]=1; unGrafo.longitud[17,16]=1000; unGrafo.longitud[17,13]=1; unGrafo.longitud[16,17]=(-10)
 conCircuito,longitud,traza=bellmanFord(unGrafo,1)
@@ -68,10 +96,13 @@ if conCircuito>0
     w=traza[w]
   end
 end
+longitud,traza=dj(unGrafo,1)
+println("traza (dj): ",traza)
+println("longitud (dj): ",longitud)
 
 #######################################################
 #
-# Uso de Bellman-Ford en el algoritmo de Ford-Fulkerson
+# Uso de caminos para el cálculo de flujos
 #
 ######################################################
 
@@ -88,7 +119,7 @@ function generarGrafoFlow(n::Int64,maxCapacidad::Int64,density::Float64)
       if rand()<(2*density) #números entre 0 y 1
         a[i,j]=0
         a[j,i]=0
-      else
+      else # aceptaremos flujo en un único sentido 
         if rand()<0.5
           a[i,j]=0
         else
@@ -103,22 +134,62 @@ end
 
 # Implementación de Ford Fulkerson
 #---------------------------------
-function fordFulkerson(g::grafoFlujos,origen::Int64,destino::Int64,s::Array{Int64,2})
+function maxFlow(g::grafoFlujos,origen::Int64,destino::Int64,s::Array{Int64,2})
   s=zeros(Int64,g.vertices,g.vertices)
+  totalFlow=0
   residual=grafoCaminos(g.vertices,zeros(Int64,g.vertices,g.vertices))
   while true
-    residual.longitud=zeros(Int64,g.vertices,g.vertices)
+    fill!(residual.longitud,g.vertices+2)
     for i in 1:g.vertices
       for j in 1:g.vertices
-        if g.capacidad[i,j]>0
-    break
+        if s[i,j]<g.capacidad[i,j]
+          residual.longitud[i,j]=1
+        end
+        if s[i,j]>0
+          residual.longitud[j,i]=1
+        end
+      end
+    end
+    longitud,traza=dj(residual,origen)
+    if longitud[destino]>(g.vertices+1)
+      # tenemos camino aumentante
+      current=destino
+      flowExtra=10000000 # mala praxis
+      while current != origen
+        if g.capacidad[traza[current],current]>0
+          remaining=g.capacidad[traza[current],current]-s[traza[current],current]
+        else
+          remaining=s[current,traza[current]]
+        end
+        if remaining<flowExtra
+          flowExtra=remaining
+        end
+        current=traza[current]
+      end
+      # ahora sabemos que el flujo aumenta en flowExtra, así que vamos sumando/restando
+      totalFlow += flowExtra
+      current=destino
+      while current != origen
+        if g.capacidad[traza[current],current]>0
+          s[traza[current],current] += flowExtra
+          assert(s[traza[current],current] <= g.capacidad[traza[current],current])
+        else
+          s[current,traza[current]] -= flowExtra
+          assert(s[current,traza[current]] >= 0)
+        end
+      end
+    else
+      # no podemos mejorar la solución actual
+      break 
+    end
   end
+  return totalFlow,s
 end
 
 unGrafo=generarGrafoFlow(25,100,0.25)
 
 solucion=Array{Int64}(unGrafo.vertices,unGrafo.vertices)
-fordFulkerson(unGrafo,1,25,solucion)
+maxFlow(unGrafo,1,25,solucion)
 #
 ##longitud,traza=djDense(unGrafo,1)
 ##println(unGrafo.arcos)
@@ -126,122 +197,4 @@ fordFulkerson(unGrafo,1,25,solucion)
 ##println(traza)
 #
 #
-########################################################
-##
-## Grafo sparse
-##
-#######################################################
-#
-#type arco
-#    origen::Int64
-#    destino::Int64
-#    longitud::Int64
-#end
-#
-#type grafoSparse
-#    nVertices::Int64
-#    nArcos::Int64
-#    pOrigen::Array{Int64,1}
-#    arcos::Array{arco,1} #arco[i,j] indica la distancia entre i y j
-#end
-#
-#function djSparse(origen::Int64,g::grafoSparse)
-#    ∞=1000000
-#    π=zeros(Int64,g.nVertices)
-#    l=Array{Int64}(g.nVertices)
-#    fill!(l,∞)
-#    l[origen]=0
-#    Q=PriorityQueue()
-#    enqueue!(Q,origen,0)
-#    while isempty(Q)==false
-#        v=dequeue!(Q)
-#        for c in g.pOrigen[v]:g.pOrigen[v+1]-1
-#            w=g.arcos[c].destino
-#            longitud=g.arcos[c].longitud
-#            if l[w] > (l[v]+longitud)
-#                l[w]=l[v]+longitud
-#                π[w]=v
-#                if haskey(Q,w)
-#                    Q[w]=l[w]
-#                else
-#                    enqueue!(Q,w,l[w])
-#                end
-#            end
-#        end
-#    end
-#    return l,π
-#end
-#
-#function generarGrafoSparse(n::Int64,p::Float64,maxValue::Int64)
-#    a=arco[]
-#    pOrigen=Int64[]
-#    nArcos=1
-#    for i in 1:n
-#        push!(pOrigen,nArcos)
-#        for j in 1:n
-#            if i!=j
-#                if rand()<p
-#                    push!(a,arco(i,j,rand(1:maxValue)))
-#                    nArcos += 1
-#                end
-#            end
-#        end
-#    end
-#    push!(pOrigen,nArcos)
-#    return grafoSparse(n,nArcos-1,pOrigen,a)
-#end
-#
-#unGrafo=generarGrafoSparse(10,0.25,1000)
-#println(unGrafo.pOrigen)
-#println(unGrafo.arcos)
-#longitud,camino=djSparse(1,unGrafo)
-#println(longitud)
-#println(camino)
-#
-########################################################
-##
-## Programación Dinámica
-##
-#######################################################
-#
-#beneficio = [ 5, 3, 2, 7, 4 ]
-#peso = [ 2, 8, 4, 2, 5 ]
-#capacidad = 10
-#
-#function knapsack(b,p,c)
-#    T=size(p,1) #número de etapas
-#    tabla = OffsetArray(Int64, 0:c, 1:T) #las filas representan la capacidad ocupada, las columnas las decisiones
-#    #etapa 1
-#    for s=1:c
-#        tabla[s,1]=(-1) #valor negativo va a indicar que no es valor posible
-#    end
-#    tabla[0,1]=0
-#    tabla[p[1],1]=b[1]
-#    #etapas 2 a T
-#    #println(tabla[:,1]) #--> permitirá explicar el funcionamiento del algoritmo
-#    for t in 2:T
-#        for s=0:c
-#            tabla[s,t]=tabla[s,t-1]
-#        end
-#        for s=c-p[t]:-1:0 #este for va de c-p[t] hasta 0 reduciendo en 1 cada vez
-#            if tabla[s,t]>=0 #contiene un valor posible
-#                if tabla[s+p[t],t]<(tabla[s,t-1]+b[t]) #mejora la opción actual?
-#                    tabla[s+p[t],t]=tabla[s,t-1]+b[t]
-#                end
-#            end
-#        end
-#        #println(tabla[:,t]) #--> permitirá explicar el funcionamiento del algoritmo
-#    end
-#    beneficio=0
-#    maxLoad=0
-#    previous=0
-#    for s in 0:c
-#        if beneficio < tabla[s,T]
-#            beneficio=tabla[s,T]
-#        end
-#    end
-#    return beneficio
-#end
-#
-#beneficio=knapsack(beneficio,peso,capacidad)
-#println(beneficio)
+
